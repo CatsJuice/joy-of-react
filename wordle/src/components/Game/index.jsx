4f2d787aa217389a3832880145d3002aab608991
CatsJuice <wp1298554944@hotmail.com>
2023-04-28 00:20:29 +0800

    feat(wordle): basic input and check

--- wordle/src/components/Game/index.jsx
+++ wordle/src/components/Game/index.jsx
@@ -26,6 +26,7 @@ function Game() {
       }))
     )
   );
+  // console.log("render Game, cursor: ", cursor.x, cursor.y);
 
   useEffect(() => {
     const handler = (e) => {
@@ -34,35 +35,35 @@ function Game() {
       const isEnter = e.key === "Enter";
       const isBackspace = e.key === "Backspace";
       const isAllowedChars = allowedChars.includes(e.key.toLowerCase());
+      const { x, y } = cursor;
+
       if (!isEnter && !isBackspace && isAllowedChars) {
-        const { x, y } = cursor;
-        if (x >= wordLength.length - 1) return;
+        if (x >= wordLength) return;
         setAnswer((prevAnswer) => {
           const nextAnswer = [...prevAnswer.map((row) => [...row])];
           nextAnswer[y][x] = { val: e.key.toUpperCase(), status: 0 };
           return nextAnswer;
         });
-        setCursor((cursor) => ({ x: cursor.x + 1, y: cursor.y }));
+        setCursor({ x: x + 1, y });
         return;
       }
       if (isBackspace) {
-        const { x, y } = cursor;
         if (x <= 0) return;
+        setCursor({ x: x - 1, y });
         setAnswer((prevAnswer) => {
           const nextAnswer = [...prevAnswer.map((row) => [...row])];
-          nextAnswer[y][x] = { val: "", status: 0 };
+          nextAnswer[y][x - 1] = { val: "", status: 0 };
           return nextAnswer;
         });
-        setCursor({ x: cursor.x - 1, y: cursor.y });
         return;
       }
-      if (isEnter && cursor[1] >= wordLength - 1) {
-        const guess = answer[cursor[0]].join("");
+      if (isEnter && x === wordLength) {
+        const guess = answer[y].join("");
         const isExits = wordDictionary.includes(guess);
         const win = guess === correct.join("");
         setAnswer((prevAnswer) => {
           const newAnswer = [...prevAnswer];
-          const currentRow = newAnswer[cursor[0]].map((cell, i) => {
+          const currentRow = newAnswer[y].map((cell, i) => {
             if (cell.val === correct[i]) {
               return { ...cell, status: 1 };
             } else if (correct.includes(cell.val)) {
@@ -71,19 +72,21 @@ function Game() {
               return { ...cell, status: 3 };
             }
           });
-          newAnswer[cursor[0]] = currentRow;
+          newAnswer[y] = currentRow;
           return newAnswer;
         });
-        cursor[0] = cursor[0] + 1;
-        cursor[1] = 0;
+        setCursor({ x: 0, y: y + 1 });
       }
     };
     window.addEventListener("keydown", handler);
     return () => window.removeEventListener("keydown", handler);
-  }, []);
+  }, [cursor, answer]);
 
   return (
     <div>
+      <div>
+        cursor: {cursor.x}, {cursor.y}
+      </div>
       <div className="grid grid-cols-5 gap-2">
         {range(chances).map((row) =>
           range(wordLength).map((col) => (
